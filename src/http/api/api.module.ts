03953b58a546b8fd65908592e3fdb91bce3daa9e
Lingu <cambagulung@gmail.com>
2021-08-05 21:36:46 +0800

    modified:   src/auth/auth.module.ts 	renamed:    src/http/auth/strategies/basic.strategy.ts -> src/auth/strategies/basic.strategy.ts 	renamed:    src/http/auth/strategies/jwt.strategy.ts -> src/auth/strategies/jwt.strategy.ts 	renamed:    src/http/auth/strategies/local.strategy.ts -> src/auth/strategies/local.strategy.ts 	modified:   src/http/api/api.module.ts 	deleted:    src/http/api/swagger/api-unauthorized-response.decorator.ts 	new file:   src/http/auth/auth.controller.spec.ts 	new file:   src/http/auth/auth.controller.ts

--- src/http/api/api.module.ts
+++ src/http/api/api.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticlesModule } from 'src/article/articles/articles.module';
+import { CategoriesModule } from 'src/article/categories/categories.module';
+import { PermissionsModule } from 'src/auth/permissions/permissions.module';
+import { RolesModule } from 'src/auth/roles/roles.module';
+import { UsersModule } from 'src/auth/users/users.module';
+import { ArticlesApiController } from './controllers/articles.controller';
+import { CategoriesApiController } from './controllers/categories.controller';
+import { PermissionsApiController } from './controllers/permissions.controller';
+import { RolesApiController } from './controllers/roles.controller';
 import { UsersApiController } from './controllers/users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [UsersApiController],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ArticlesModule,
+    CategoriesModule,
+    PermissionsModule,
+    RolesModule,
+    UsersModule,
+  ],
+  controllers: [
+    ArticlesApiController,
+    CategoriesApiController,
+    PermissionsApiController,
+    RolesApiController,
+    UsersApiController,
+    UsersApiController,
+  ],
 })
 export class ApiModule {}
